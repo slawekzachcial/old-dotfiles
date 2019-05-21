d72988b9d9fd3e9584be433950eed659d285709b
Slawek Zachcial <slawek.zachcial@gmail.com>
2019-05-21 08:08:09 +0200

    Fix tmux theme

--- tmux/theme.sh
+++ tmux/theme.sh
@@ -18,28 +18,22 @@ set-option -g status-interval 5
 
 
 # default statusbar colors
-# set-option -g status-bg colour0
-set-option -g status-fg $tm_color_active
-set-option -g status-bg default
-set-option -g status-attr default
+set-option -g status-style fg=$tm_color_active,bg=default
 
 # default window title colors
-set-window-option -g window-status-fg $tm_color_inactive
-set-window-option -g window-status-bg default
+set-window-option -g window-status-style fg=$tm_color_inactive,bg=default
 set-option -g window-status-format "#I #{?window_zoomed_flag,(,}#W#{?window_zoomed_flag,),}"
 
 # active window title colors
-set-window-option -g window-status-current-fg $tm_color_active
-set-window-option -g window-status-current-bg default
+set-window-option -g window-status-current-style fg=$tm_color_active,bg=default
 set-window-option -g window-status-current-format "#[bold]#I #{?window_zoomed_flag,(,}#W#{?window_zoomed_flag,),}"
 
 # pane border
-set-option -g pane-border-fg $tm_color_inactive
-set-option -g pane-active-border-fg $tm_color_active
+set-option -g pane-border-style fg=$tm_color_inactive
+set-option -g pane-active-border-style fg=$tm_color_active
 
 # message text
-set-option -g message-bg default
-set-option -g message-fg $tm_color_active
+set-option -g message-style bg=default,fg=$tm_color_active
 
 # pane number display
 set-option -g display-panes-active-colour $tm_color_active
